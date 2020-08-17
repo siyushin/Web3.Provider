b02c337a4bf24f5d6680f05f6c10023c3945c2fe
Yushin Si <siyushin@hotmail.com>
2020-08-17 13:58:50 +0800

    完成签名和交易两个功能。

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import HttpProvider from 'web3-providers-http'
 
 class ElaphantWeb3Provider extends HttpProvider {
 	constructor(rpcURL, appTitle, appID, appName, appPublicKey, developerDID) {
-		console.log('参数：', rpcURL, appTitle, appID, appName, appPublicKey, developerDID)
 		super(rpcURL, {
 			keepAlive: true,
 			withCredentials: false,
@@ -53,14 +52,9 @@ class ElaphantWeb3Provider extends HttpProvider {
 			enable: function () {
 				let currentURL = window.location.href
 				let itsURL = new URL(currentURL)
-				let data = itsURL.searchParams.get('Data')
-				if (data) {
-					// BTCAddress: "12iekRGdkJAg42vDVu3R9HFrzKA3Y2asTb"
-					// DID: "idNSQQ6CaXQEGHB7QeqcD12zeu7UB8Acs4"
-					// ELAAddress: "ENma6kd2r4qKR7Qa3Q2vRrZkNC4GPrEQiF"
-					// ETHAddress: "0xBAE4229e6d7D7404c9AaE150ccA99ae53FebAdBF"
-					// PublicKey: "03ddd9b47aa1fd44cf6125134c4a782e1be34165f71ea04cc5ab232835eaf82730"
-					// RandomNumber: "26587889"
+				let action = itsURL.searchParams.get('action')
+				if (action && action === 'auth') {
+					let data = itsURL.searchParams.get('Data')
 					let dataJson = JSON.parse(decodeURIComponent(data))
 					this.selectedAddress = dataJson.ETHAddress
 
@@ -68,29 +62,62 @@ class ElaphantWeb3Provider extends HttpProvider {
 						return resolve(this.selectedAddress)
 					})
 				} else {
-					return new Promise((resolve, reject) => {
-						let elaphantURL = "elaphant://identity?" +
-							"AppID=" + this.provider.appID +
-							"&AppName=" + encodeURIComponent(this.provider.appName) +
-							"&RandomNumber=" + Math.floor(Math.random() * 100000000) +
-							"&DID=" + this.provider.developerDID +
-							"&PublicKey=" + this.provider.appPublicKey +
-							"&ReturnUrl=" + encodeURIComponent(currentURL) +
-							"&RequestInfo=ELAAddress,BTCAddress,ETHAddress"
-						let url = "https://launch.elaphant.app/?appName=" + encodeURIComponent(this.provider.appTitle) +
-							"&appTitle=" + encodeURIComponent(this.provider.appTitle) +
-							"&autoRedirect=True&redirectURL=" + encodeURIComponent(elaphantURL)
-						// window.open(url, "_blank")
-						window.location.href = url
-					})
+					itsURL.searchParams.set('action', 'auth')
+
+					let elaphantURL = "elaphant://identity?" +
+						"AppID=" + this.provider.appID +
+						"&AppName=" + encodeURIComponent(this.provider.appName) +
+						"&RandomNumber=" + Math.floor(Math.random() * 100000000) +
+						"&DID=" + this.provider.developerDID +
+						"&PublicKey=" + this.provider.appPublicKey +
+						"&ReturnUrl=" + encodeURIComponent(itsURL.toString()) +
+						"&RequestInfo=ELAAddress,BTCAddress,ETHAddress"
+					let url = "https://launch.elaphant.app/?appName=" + encodeURIComponent(this.provider.appTitle) +
+						"&appTitle=" + encodeURIComponent(this.provider.appTitle) +
+						"&autoRedirect=True&redirectURL=" + encodeURIComponent(elaphantURL)
+					window.location.href = url
 				}
 			}
 		}
 	}
 
 	send(payload, callback) {
+		if (payload.method === 'eth_sendTransaction') {
+			return this.sendTransaction(payload.params)
+		}
+
 		super.send(payload, callback)
-		// console.log(payload)
+		console.log('payload =', payload)
+	}
+
+	sendTransaction(args) {
+		let returnUrl = new URL(window.location.href)
+		if (returnUrl.searchParams.get('action') === 'auth') {
+			returnUrl.searchParams.delete('Data')
+			returnUrl.searchParams.delete('Sign')
+		}
+		returnUrl.searchParams.set('action', 'tx')
+
+		let arg = args[0]
+		let orderID = "";
+		let elaphantURL = "elaphant://calleth?DID=" + this.developerDID +
+			"&AppID=" + this.appID +
+			"&AppName=" + encodeURIComponent(this.appName) +
+			"&Description=" + encodeURIComponent(this.appName) +
+			"&PublicKey=" + this.appPublicKey +
+			"&OrderID=" + orderID +
+			"&CoinName=Ethsc" +
+			"&to=" + arg.to +
+			"&value=" + parseInt(arg.value) +
+			"&price=" + parseInt(arg.gasPrice) +
+			"&gas=" + parseInt(arg.gas) +
+			"&data=" + arg.data +
+			"&ReturnUrl=" + encodeURIComponent(returnUrl.toString());
+
+		let url = "https://launch.elaphant.app/?appName=" + encodeURIComponent(this.appTitle) +
+			"&appTitle=" + encodeURIComponent(this.appTitle) +
+			"&autoRedirect=True&redirectURL=" + encodeURIComponent(elaphantURL);
+		window.location.href = url;
 	}
 }
 
